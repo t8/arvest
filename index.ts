ae725499b27ed055bfc5ce7c3fc891eeca3f5db8
tate <tateflyer1@gmail.com>
2020-10-30 15:13:34 -0400

    chore: function comments

--- index.ts
+++ index.ts
@@ -14,20 +14,32 @@ export interface config {
 // const keyfile = JSON.parse(process.env.KEYFILE);
 const keyfile: JWKInterface = {kty: "", e: "", n: ""};
 
+/**
+ * The total number of days for tokens to be locked
+ */
 function totalVestingDays(): number {
   const now = dayjs();
   const then = now.add(config.vest_period, "year");
   return then.diff(now, "day");
 }
 
+/**
+ * The total number of blocks for tokens to be locked
+ */
 function totalVestingBlocks(): number {
   return totalVestingDays() * 30;
 }
 
+/**
+ * The number of tokens to be unlocked at any given release time
+ */
 function tokensReleased(): number {
   return Math.round(config.tokens_to_vest / totalVestingDays());
 }
 
+/**
+ * Generates a list of block heights where tokens are to be released
+ */
 function generateBlockHeights(): number[] {
   let blockHeights = new Array(totalVestingBlocks() / 30);
   for (let i = 0; i < blockHeights.length; i++) {
@@ -40,6 +52,11 @@ function generateBlockHeights(): number[] {
   return blockHeights;
 }
 
+/**
+ * Generates Arweave transactions for each scheduled release
+ * @param blockHeights block heights release schedule
+ * @returns array of Arweave transactions
+ */
 async function generateTransactions(blockHeights: number[]): Promise<Transaction[]> {
   const client = Arweave.init({
     host: 'arweave.net',
