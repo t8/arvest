83498df15ceea77f8c84a0af0c4e54eb9d276b59
tate <tateflyer1@gmail.com>
2020-10-30 01:23:09 -0400

    feat: generate transactions (WIP)

--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
 import config from './config';
 import dayjs from 'dayjs';
+import Arweave from 'arweave';
+import Transaction from 'arweave/node/lib/transaction';
+import { JWKInterface } from 'arweave/node/lib/wallet';
 dayjs().format();
 
 export interface config {
@@ -9,6 +12,7 @@ export interface config {
 }
 
 // const keyfile = JSON.parse(process.env.KEYFILE);
+const keyfile: JWKInterface = JSON.parse(process.env.KEYFILE);
 
 function totalVestingDays(): number {
   const now = dayjs();
@@ -22,6 +26,38 @@ function totalVestingBlocks(): number {
 
 function tokensReleased(): number {
   return Math.round(config.tokens_to_vest / totalVestingDays());
+}
+
+function generateBlockHeights(): number[] {
+  let blockHeights = [];
+  for (let i = 0; i < totalVestingBlocks() / 30; i++) {
+    if (i = 0) {
+      blockHeights[i] = 30;
+    } else {
+      blockHeights[i] = blockHeights[i - 1] + 30;
+    }
+  }
+  return blockHeights;
+}
+
+async function generateTransactions(blockHeights: number[]): Promise<Transaction[]> {
+  const client = Arweave.init({
+    host: 'arweave.net',
+    port: 443,
+    protocol: 'https',
+    timeout: 20000,
+    logging: false,
+  });
+  let transactions = [];
+
+  for (let i = 0; i < blockHeights.length; i++) {
+    transactions[i] = await client.createTransaction({
+      data: Math.random().toString().slice(1, 4)
+    }, keyfile);
+    transactions[i].addTag("Application", "ArVest");
+  }
+
+  return transactions;
 }
 
 console.log(totalVestingDays());
